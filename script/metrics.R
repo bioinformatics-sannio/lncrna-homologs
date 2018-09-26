dceef4d4cdef2a40c0bee3a77e816cd069974229
Teresa <teresa@Fishers-iMac.local>
2018-09-26 16:17:36 +0200

    update

--- script/metrics.R
+++ script/metrics.R
@@ -1,3 +1,12 @@
+# Please install the following Python package and C++ source before proceeding:
+# - decaf+py from http://bioinformatics.org.au/tools/decaf+py/
+# - kmacs from http://kmacs.gobics.de/
+#
+# References
+# - Höhl, M., Rigoutsos, I., & Ragan, M. A. (2006). Pattern-based phylogenetic distance estimation and tree reconstruction. Evolutionary Bioinformatics, 2, 117693430600200016.
+# - C.-A. Leimeister, B. Morgenstern (2014). kmacs: the k-Mismatch Average Common Substring Approach to alignment-free sequence comparison. Bioinformatics 30, 2000-2008
+# - S. Horwege, S. Lindner, M. Boden, K. Hatje, M. Kollmar, C.-A. Leimeister, B. Morgenstern (2014). Spaced words and kmacs: fast alignment-free sequence comparison based on inexact word matches. Nuc. Acids Research 42, W7-W11
+
 
 BBC = function(S1,S2){
   W.S2 = dinucleotideFrequency(S2)/(length(S2)-2 +1)
